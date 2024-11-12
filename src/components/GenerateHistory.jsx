a2b200dcda0571e0d6cdc7c07663a72d0a653ff2
DarthVader94 <dart.veidar827@yandex.r>
2024-11-12 10:56:54 +0300

    fixed generator history

--- src/components/GenerateHistory.jsx
+++ src/components/GenerateHistory.jsx
@@ -8,7 +8,6 @@ export const GenerateHistory = () => {
 
    return (
       <div>
-         //!Изменения
          {date.map((text) => (
             <p className={s.history__scan} key={text}>{text}
                <QRCodeSVG value={text} size={100} />
